90e06b12ede56e39ec3966625aac68ccfff32e6c
Jaweed Ina <114490455+InaJaweed@users.noreply.github.com>
2024-08-07 22:53:01 +0100

    added comments

--- String - Palindrome/main.c
+++ String - Palindrome/main.c
@@ -1,21 +1,25 @@
 #include <stdio.h>
 
+// Define maximum length for input string
 #define MAX_LEN 100
+
+// Define the set of punctuation characters to be removed
 const char punctuations[] = ".,!?;:\"'()[]{}<>-@#$%^&*_+=|\\/~`";
 
-size_t StringLen(const char* str){
+// Function to calculate the length of a string
+size_t StringLen(const char* str) {
     size_t count = 0;
-    while(str[count] != '\0'){
+    while(str[count] != '\0') {
         count++;
     }
-
     return count;
 }
 
-void StringReverse(char* str){
+// Function to reverse a string in place
+void StringReverse(char* str) {
     size_t len = StringLen(str);
-    if(len > 0){
-        for(size_t i = 0; i < len / 2; i++){
+    if(len > 0) {
+        for(size_t i = 0; i < len / 2; i++) {
             char temp = str[i];
             str[i] = str[len - 1 - i];
             str[len - 1 - i] = temp;
@@ -23,43 +27,42 @@ void StringReverse(char* str){
     }
 }
 
-// Function to check if a character is punctuation
-int is_punctuation(char ch, const char* punct){
-    for(int i = 0; punct[i] != '\0'; i++){
-        if(ch == punct[i]){
+// Function to check if a character is a punctuation mark
+int is_punctuation(char ch, const char* punct) {
+    for(int i = 0; punct[i] != '\0'; i++) {
+        if(ch == punct[i]) {
             return 1;
         }
     }
     return 0;
 }
 
-// Function to remove punctuation
-void remove_punctuation(char* str, const char* punct, char* punctRem){
+// Function to remove punctuation from a string
+void remove_punctuation(char* str, const char* punct, char* punctRem) {
     int j = 0;
-    for (int i = 0; str[i] != '\0'; i++){
-        if(!is_punctuation(str[i], punct)){
+    for (int i = 0; str[i] != '\0'; i++) {
+        if(!is_punctuation(str[i], punct)) {
             punctRem[j++] = str[i];
         }
     }
-
     punctRem[j] = '\0';
 }
 
-// Function to get string to lowercase
-void lowercase(char* str){
-    while(*str != '\0'){
-        if(*str >= 65 && *str <= 90){
-            *str += 32;
+// Function to convert a string to lowercase
+void lowercase(char* str) {
+    while(*str != '\0') {
+        if(*str >= 65 && *str <= 90) {  // ASCII values for 'A' to 'Z'
+            *str += 32;  // Convert to corresponding lowercase letter
         }
         str++;
     }
 }
 
-char* removeSpace(char* str){
+// Function to remove spaces from a string
+char* removeSpace(char* str) {
     size_t non_space_count = 0;
-
-    for(size_t i = 0; str[i] != '\0'; i++){
-        if(str[i] != ' '){
+    for(size_t i = 0; str[i] != '\0'; i++) {
+        if(str[i] != ' ') {
             str[non_space_count] = str[i];
             non_space_count++;
         }
@@ -68,31 +71,30 @@ char* removeSpace(char* str){
     return str;
 }
 
-int strPali(const char* str1, const char* str2){
-    while(*str1 != '\0' && *str2 != '\0'){
-        if(*str1 != *str2){
+// Function to check if two strings are identical
+int strPali(const char* str1, const char* str2) {
+    while(*str1 != '\0' && *str2 != '\0') {
+        if(*str1 != *str2) {
             return 0;
         }
-
         str1++;
         str2++;
     }
-
     return (*str1 == '\0' && *str2 == '\0');
 }
 
-int main(void){
+int main(void) {
     char userInput[MAX_LEN];
     char punctRemoved[MAX_LEN];
 
     printf("Enter a string: ");
     
     // Read user string input
-    if(fgets(userInput, MAX_LEN, stdin) != NULL){
+    if(fgets(userInput, MAX_LEN, stdin) != NULL) {
         // Calculate string length
         size_t len = StringLen(userInput);
-        // Remove new character line if present 
-        if(len > 0 && userInput[len - 1] == '\n'){
+        // Remove newline character if present
+        if(len > 0 && userInput[len - 1] == '\n') {
             userInput[len - 1] = '\0';
             len--;
         }
@@ -101,23 +103,24 @@ int main(void){
         printf("Original String: %s\n", userInput);
         
         /*
-            Functions to remove white space and then make sure all char is lowercase, and then remove punctuations
+            Functions to remove white space, convert all characters to lowercase,
+            and then remove punctuations
         */
-        lowercase(removeSpace(userInput)); // Remove WhiteSpace and Make all char lowercase
-        remove_punctuation(userInput, punctuations, punctRemoved); // remove punctuation
-        remove_punctuation(punctRemoved, punctuations, userInput); // remove punctuation
-        StringReverse(punctRemoved); // Reverse String
+        lowercase(removeSpace(userInput));  // Remove white space and convert to lowercase
+        remove_punctuation(userInput, punctuations, punctRemoved);  // Remove punctuation and add it punctRemoved array
+        remove_punctuation(punctRemoved, punctuations, userInput);  // Remove punctuation from oringal string
+        StringReverse(punctRemoved);  // Reverse the string
 
         printf("String After Modification: %s\n", punctRemoved);
         printf("Original String Modified: %s\n", userInput);
-        int isPali = strPali(userInput, punctRemoved);
 
-        if(isPali){
+        // Check if the original modified string is a palindrome
+        int isPali = strPali(userInput, punctRemoved);
+        if(isPali) {
             printf("The string is a palindrome\n");
-        }else{
+        } else {
             printf("The string is NOT a palindrome\n");
         }
-
     }
 
     return 0;
